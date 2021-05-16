602a68a6c6ba618cb7a4a7eb7dce6f2c9db4f62b
Vinson <windshg@126.com>
2021-05-16 14:36:38 +0800

    Update Add Two Numbers.cpp

--- Add Two Numbers.cpp
+++ Add Two Numbers.cpp
@@ -3,41 +3,43 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        int carry = 0;
-        ListNode dh(0);
-        ListNode *cur = &dh;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dh;
+        ListNode* pre = &dh;
+        
+        int ac = 0;
         while (l1 && l2) {
-            int t = l1->val + l2->val + carry;
-            cur->next = new ListNode(t % 10);
-            carry = t / 10;
-            cur = cur->next;
+            int sum = l1->val + l2->val + ac;
+            ListNode* n = new ListNode(sum % 10, nullptr);
+            ac = sum / 10;
+            pre->next = n;
+            pre = pre->next;
             l1 = l1->next;
             l2 = l2->next;
         }
-        while (l1) {
-            int t = l1->val + carry;
-            cur->next = new ListNode(t % 10);
-            carry = t / 10;
-            cur = cur->next;
-            l1 = l1->next;
+        
+        ListNode* l = l1;
+        if (l2) l = l2;
+
+        while (l) {
+            int sum = l->val + ac;
+            ListNode* n = new ListNode;
+            n->val = sum % 10;
+            ac = sum / 10;
+            pre->next = n;
+            pre = pre->next;
+            l = l->next;
         }
-        while (l2) {
-            int t = l2->val + carry;
-            cur->next = new ListNode(t % 10);
-            carry = t / 10;
-            cur = cur->next;
-            l2 = l2->next;
-        }
-        if (carry) {
-            cur->next = new ListNode(carry);
-            cur = cur->next;
-        }
+
+        if (ac > 0) pre->next = new ListNode(ac, nullptr);
+
         return dh.next;
     }
 };
